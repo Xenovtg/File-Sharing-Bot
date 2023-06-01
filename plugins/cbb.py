d29f2264cc8a26438345a92776984574acbda8c3
Xenovtg <135080363+Xenovtg@users.noreply.github.com>
2023-06-01 19:41:07 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,6 +1,5 @@
 #(©)Codexbotz
 
-from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -10,7 +9,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://telegram.me/sn_botsupport'>Ask here</a>\n○ Channel : @Anime_Supernova\n○ Support Group : @SN_BotSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Developer : @Xenov7x\n○ Language : <code>Python3</code>\n○ Source Code : <a href='https://telegram.me/sn_botsupport'>Ask here</a>\n○ Channel : @Anime_Supernova\n○ Support Group : @SN_BotSupport</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
